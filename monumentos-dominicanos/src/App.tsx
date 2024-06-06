101322f09f6d4aa382a2fd089bbf1d94a1e1ac96
Wilgrey2004 <167430214+Wilgrey2004@users.noreply.github.com>
2024-06-06 14:25:52 -0500

    Version final
    Esta es la ultima versión del proyecto

--- monumentos-dominicanos/src/App.tsx
+++ monumentos-dominicanos/src/App.tsx
@@ -1,19 +1,15 @@
 import "./App.css";
 import { PContainer } from "./Components/ContenedorPrincipal/PContainer";
 import { NavBar } from "./Components/NavBar/NavBar";
-import { Footer } from "./Components/Footer/Footer";
 
 function App() {
   return (
     <>
       <div className="overflow-auto w-full flex justify-center h-full flex-col ">
-        <div className="order-3">
-          <NavBar></NavBar>
-        </div>
-        <main className=" order-2">
+        <NavBar></NavBar>
+        <main className="">
           <PContainer />
         </main>
-        <div className="order-1"></div>
       </div>
     </>
   );
--- monumentos-dominicanos/src/Components/Footer/Footer.tsx
+++ monumentos-dominicanos/src/Components/Footer/Footer.tsx
@@ -2,9 +2,13 @@ import { Contacs } from "../contacts/Contacs";
 
 export const Footer = () => {
   return (
-    <footer className=" w-full h-[20%]  -shadow-md p-8  bg-slate-200  flex items-center flex-col">
-      <span className=" text-xl ">
-        Creado por <h3 className="inline font-bold"> Wilgrey Ravelo Cruz</h3>
+    <footer className="w-full h-[20%]  -shadow-md p-8  bg-slate-200  flex items-center flex-col">
+      <span className=" hover:-translate-y-1  transition-all  hover:shadow-lg  hover:shadow-white text-xl   bg-gradient-to-b from-blue-300 to-red-300 border-blue-400  rounded-2xl p-2 ">
+        Creado por{" "}
+        <h3 className="inline font-bold font-serif hover:-scale-105 duration-100  transition-all  hover:text-blue-500">
+          {" "}
+          Wilgrey Ravelo Cruz
+        </h3>
       </span>
       <p>Contactame</p>
       <Contacs />
